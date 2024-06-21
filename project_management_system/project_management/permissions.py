6540683ef96510ce08e8a0e99185876eeac3b287
saim2001 <saimrao49@gmail.com>
2024-06-21 05:27:37 +0500

    ready

--- project_management_system/project_management/permissions.py
+++ project_management_system/project_management/permissions.py
@@ -1,34 +1,85 @@
 # permissions.py
-from rest_framework.permissions import BasePermission,SAFE_METHODS
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from .models import Project, ProjectPermission
 from django.conf import settings
 
+
 class IsProjectOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-       
-        # Allow read-only access for non-authenticated users
-        if request.method in SAFE_METHODS:
+        # Allow list and create actions without checking for a pk
+        print(view.kwargs)
+        if view.action in ['list', 'create']:
             return True
-        # Check if the user is the owner of the project
-        project = Project.objects.get(id=view.kwargs['pk'])
-        return project.created_by == request.user
+
+        # For detail views, check for permissions based on 'pk'
+        if 'pk' in view.kwargs:
+            try:
+                project = Project.objects.get(id=view.kwargs['pk'])
+            except Project.DoesNotExist:
+                return False
+
+            # Allow if the user is the creator of the project
+            if project.created_by == request.user:
+                return True
+
+            # Otherwise, check specific permissions
+            permission = ProjectPermission.objects.filter(
+                project=project,
+                user=request.user
+            ).first()
+
+            if permission:
+                if view.action == 'retrieve' and permission.can_view:
+                    return True
+                if view.action == 'update' and permission.can_add_users:
+                    return True
+
+        return False
+
 
 class HasProjectPermission(BasePermission):
     def has_permission(self, request, view):
-        project = Project.objects.get(id=view.kwargs['pk'])
-        permission = ProjectPermission.objects.get(project=project, user=request.user)
+
         if request.method in SAFE_METHODS:
             return True
-        if request.method == 'POST' and permission.can_create:
+
+    # For other actions, check for project-specific permissions
+        project_id = request.data['project']
+
+        try:
+            print(project_id)
+            project = Project.objects.get(id=project_id)
+            print(request.user, project.created_by)
+            if request.user == project.created_by:
+                return True
+            permission = ProjectPermission.objects.get(
+                project=project, user=request.user)
+        except Project.DoesNotExist:
+            return False
+        except ProjectPermission.DoesNotExist:
+            return False
+
+        if request.method == "POST" and permission.can_create:
             return True
+
         if request.method in ['PUT', 'PATCH'] and permission.can_edit:
             return True
         if request.method == 'DELETE' and permission.can_delete:
             return True
         return False
 
+
+class CanAddPermissions(BasePermission):
+    def has_permission(self, request, view):
+        print(view.kwargs, request.user.id)
+        project = Project.objects.get(id=request.data['project'])
+        print(project.created_by)
+        return project.created_by == request.user
+
+
 class CanAddUsers(BasePermission):
     def has_permission(self, request, view):
         project = Project.objects.get(id=view.kwargs['pk'])
-        permission = ProjectPermission.objects.get(project=project, user=request.user)
+        permission = ProjectPermission.objects.get(
+            project=project, user=request.user)
         return permission.can_add_users
--- project_management_system/project_management/views.py
+++ project_management_system/project_management/views.py
@@ -1,45 +1,89 @@
-from rest_framework import generics,viewsets
+from rest_framework import generics, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.permissions import IsAuthenticated
-from .models import Project,Task,ProjectPermission
-from .permissions import IsProjectOwnerOrReadOnly,HasProjectPermission,CanAddUsers
-from .serializer import UserSerializer,MyTokenObtainPairSerializer,ProjectSerializer,TaskSerializer,ProjectPermissionSerializer
+from .models import Project, Task, ProjectPermission
+from .permissions import IsProjectOwnerOrReadOnly, HasProjectPermission, CanAddUsers, CanAddPermissions
+from .serializer import UserSerializer, MyTokenObtainPairSerializer, ProjectSerializer, TaskSerializer, ProjectPermissionSerializer
 from django.contrib.auth import get_user_model
 
-User =  get_user_model()
+User = get_user_model()
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.filter(is_deleted=False)
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated,IsProjectOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated, IsProjectOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        users = serializer.validated_data.get('users', [])
+        user = self.request.user
+        users.append(user)
+        project = serializer.save(created_by=user)
+        project.users.set(users)
+        for i in users:
+            if i.id != user.id:
+                part_usr = User.objects.get(id=i.id)
+                permissions = ProjectPermission.objects.create(
+                    project=project, user=part_usr)
+                permissions.save()
 
     def perform_destroy(self, instance):
         instance.is_deleted = True
         instance.save()
-    
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.filter(is_deleted=False)
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated,HasProjectPermission]
+    permission_classes = [IsAuthenticated, HasProjectPermission]
+
+    # def perform_create(self, serializer):
+    #     project_id = serializer.validated_data.get('project').id
+    #     user_id = self.request.user.id
+    #     try:
+    #         project = Project.objects.get(id=project_id)
+    #     except Project.DoesNotExist:
+    #         raise NotFound('Project not found')
+    #     print(self.request.user == project.created_by)
+    #     if self.request.user == project.created_by:
+    #         serializer.save()
+    #     else:
+    #         try:
+    #             project_permission = ProjectPermission.objects.get(
+    #                 project_id=project_id, user_id=user_id)
+    #             if project_permission.can_create:
+    #                 serializer.save()
+    #             else:
+    #                 raise PermissionDenied(
+    #                     'You do not have permission to create task')
+    #         except ProjectPermission.DoesNotExist:
+    #             raise PermissionDenied(
+    #                 'You do not have permission to create task')
 
     def perform_destroy(self, instance):
+
         instance.is_deleted = True
         instance.save()
 
-class ProjectPermissionView(generics.CreateAPIView):
+
+class ProjectPermissionViewSet(viewsets.ModelViewSet):
     queryset = ProjectPermission.objects.all()
     serializer_class = ProjectPermissionSerializer
-    permission_classes = [IsAuthenticated, CanAddUsers]
-    
+    permission_classes = [IsAuthenticated, CanAddPermissions]
 
-
+    def perform_create(self, serializer):
+        project = Project.objects.get(id=self.request.data['project'])
+        serializer.save(project=project)
 
 
 # Create your views here.
